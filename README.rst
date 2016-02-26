ffc326397c44728a7b43d11937d12214e23d7887
Sam Mulube <sam@umbrellium.co.uk>
2016-02-26 15:36:25 +0000

    Initial import of hypercat library from HyperCat/IoT/python_tools repo
    https://github.com/HyperCatIoT/python-tools

---
+++ README.rst
@@ -0,0 +1,142 @@
+Hypercat.py
+===========
+
+A minimal library for working with Hypercat 3.0 catalogues.
+
+Use cases
+---------
+
+Servers:
+
+* Create an empty Hypercat catalogue
+* Optionally, add metadata to describe it
+* Optionally, add items to the catalogue
+
+  * an item is either another catalogue or a resource
+
+* Output it as JSON, either minimally or prettyprinted
+* Find a specific part of a catalogue hierarchy
+
+Clients:
+
+* Load and validate a Hypercat
+
+Example - Hypercat server
+-------------------------
+
+We'll create a Hypercat catalogue with 2 items in it::
+
+      h
+     / \
+    h2  r
+
+Usage::
+
+    from hypercat import hypercat
+
+    # Create a catalogue
+    h = hypercat.hypercat("CatalogueContainingOneCatalogueAndOneResource")
+
+    # Create a second catalogue, and add it as a child of the first
+    h2 = hypercat.hypercat("ChildCatalogue")
+    h.addItem(h2, "/child_cat")
+
+    # Create a resource, and add it as another child of the first catalogue
+    r = hypercat.resource("resource1", "application/vnd.hypercat.sensordata+json")
+    h.addItem(r, "/resource")
+
+    # Print the raw JSON of the catalogue, and then with human-friendly formatting
+    print h.asJSON()
+    print h.prettyprint()
+
+This should output::
+
+    {
+        "catalogue-metadata": [
+            {
+                "rel": "urn:X-hypercat:rels:isContentType",
+                "val": "application/vnd.hypercat.catalogue+json"
+            },
+            {
+                "rel": "urn:X-hypercat:rels:hasDescription:en",
+                "val": "CatalogueContainingOneCatalogueAndOneResource"
+            }
+        ],
+        "items": [
+            {
+                "href": "/child_cat",
+                "item-metadata": [
+                    {
+                        "rel": "urn:X-hypercat:rels:isContentType",
+                        "val": "application/vnd.hypercat.catalogue+json"
+                    },
+                    {
+                        "rel": "urn:X-hypercat:rels:hasDescription:en",
+                        "val": "ChildCatalogue"
+                    }
+                ]
+            },
+            {
+                "href": "/resource",
+                "item-metadata": [
+                    {
+                        "rel": "urn:X-hypercat:rels:isContentType",
+                        "val": "application/vnd.hypercat.sensordata+json"
+                    },
+                    {
+                        "rel": "urn:X-hypercat:rels:hasDescription:en",
+                        "val": "resource1"
+                    }
+                ]
+            }
+        ]
+    }
+
+
+Example - Hypercat Client
+-------------------------
+
+Usage::
+
+    from hypercat import hypercat
+
+    h = hypercat.loads(inString)  # Read-in and validate Hypercat
+    print "Metadata is ",h.metadata
+
+How this module works
+=====================
+
+According to the spec, each Catalogue has a (human-readable) description and a
+list of metadata about it.  It also contains a list of "items", and each item
+has an HREF and a list of metadata about it.  An item can be any kind of
+resource, including another catalogue.
+
+So conceptually, catalogues can have many levels of hierarchy (i.e. a catalogue
+can contain a catalogue which contains a catalogue and so on). Catalogues don't
+just have to be trees either, they can be graphs, contain loops etc., and
+clients of this module will often want to declare full catalogue structures
+several levels deep, i.e. build their entire hierarchy in one go.
+
+But according to the spec only one level of catalogue can be output at a time,
+i.e. getting a catalogue will declare its child catalogues, but not its
+grand-children (to see the grand-children, you'd have to get the child
+catalogue)
+
+A further complication is some asymmetry in how attributes are declared when a
+catalogue is the parent, vs. when it is the child
+
+To deal with this, within this module we maintain a universal base class for
+every hypercat object.  Then during output, we ignore grand-children, and
+modify attributes as necessary.
+
+License
+=======
+
+The software is released under an MIT license. Please see the details in
+LICENSE.txt.
+
+TODO
+====
+
+4.3.3 Says that it is optional to use isContentType to tag each member of
+items[], however we treat it here as mandatory.
---
+++ tests/test_hypercat.py
@@ -0,0 +1,209 @@
+# UNITTEST.PY
+# Copyright (c) 2013 Pilgrim Beart <firstname.lastname@1248.io>
+#
+# Simple unit-tests for hypercat.py
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from hypercat import hypercat
+
+def unittest():
+    print "Running unit tests"
+
+    print "\nTEST: Create minimal empty Catalogue, and render to a string, minimally, then with pretty-printing"
+    h = hypercat.Hypercat("")
+
+    s = h.asJSONstr()
+    print s
+    assert s=="""{"catalogue-metadata":[{"rel":"urn:X-hypercat:rels:isContentType","val":"application/vnd.hypercat.catalogue+json"},{"rel":"urn:X-hypercat:rels:hasDescription:en","val":""}],"items":[]}"""
+
+    result = h.prettyprint()
+    print result
+    assert result=="""{
+    "catalogue-metadata": [
+        {
+            "rel": "urn:X-hypercat:rels:isContentType",
+            "val": "application/vnd.hypercat.catalogue+json"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:hasDescription:en",
+            "val": ""
+        }
+    ],
+    "items": []
+}"""
+
+    print "\nTEST: Create a catalogue containing 1 catalogue and 1 resource, held as data"
+    h = hypercat.Hypercat("CatalogueContainingOneCatalogueAndOneResource")
+    h2 = hypercat.Hypercat("ChildCatalogue")
+    print "about to add child catalogue"
+    h.addItem(h2, "http://FIXMEcat")
+    r = hypercat.Resource("resource1", "application/vnd.hypercat.sensordata+json")
+    print "about to add child resource"
+    h.addItem(r, "http://FIXMEresource")
+    result = h.asJSON()
+    print result
+    print h.prettyprint()
+    assert result=={'items': [{'item-metadata': [{'val': 'application/vnd.hypercat.catalogue+json', 'rel': 'urn:X-hypercat:rels:isContentType'}, {'val': 'ChildCatalogue', 'rel': 'urn:X-hypercat:rels:hasDescription:en'}], 'href': 'http://FIXMEcat'}, {'item-metadata': [{'val': 'application/vnd.hypercat.sensordata+json', 'rel': 'urn:X-hypercat:rels:isContentType'}, {'val': 'resource1', 'rel': 'urn:X-hypercat:rels:hasDescription:en'}], 'href': 'http://FIXMEresource'}], 'catalogue-metadata': [{'val': 'application/vnd.hypercat.catalogue+json', 'rel': 'urn:X-hypercat:rels:isContentType'}, {'val': 'CatalogueContainingOneCatalogueAndOneResource', 'rel': 'urn:X-hypercat:rels:hasDescription:en'}]}
+
+    print "\nTEST: Create a catalogue 2 deep (and output each level)"
+    h1 = hypercat.Hypercat("Top")
+    h2 = hypercat.Hypercat("Middle")
+    h3 = hypercat.Hypercat("Bottom")
+    h1.addItem(h2, "http://FIXMEcat2")
+    h2.addItem(h3, "http://FIXMEcat3")
+    print "Top:"
+    print h1.asJSON()
+    print h1.prettyprint()
+    print "Middle:"
+    print h2.asJSON()
+    print h2.prettyprint()
+    print "Bottom:"
+    print h3.asJSON()
+    print h3.prettyprint()
+
+    print "\nTEST: Creating more than 2 levels of catalogue, then outputting different levels"
+    h1 = hypercat.Hypercat("Top")
+    h1.addRelation("name","top")
+    h2 = hypercat.Hypercat("Middle")
+    h2.addRelation("name","middle")
+    h3 = hypercat.Hypercat("Bottom")
+    h3.addRelation("name","bottom")
+    h1.addItem(h2, "http://FIXMEcat2")
+    h2.addItem(h3, "http://FIXMEcat3")
+
+    print "Find top catalogue:"
+    hN = h1.findByPath("name", "/")
+    print hN.prettyprint()
+    assert hN.values("name")[0] == "top"
+
+    print "Find middle catalogue:"
+    hN = h1.findByPath("name", "/middle/")
+    print hN.prettyprint()
+    assert hN.values("name")[0] == "middle"
+
+    print "Find bottom catalogue:"
+    hN = h1.findByPath("name", "/middle/bottom")
+    print hN.prettyprint()
+    assert hN.values("name")[0] == "bottom"
+
+    print "\nTEST: Create a fancy Catalogue with optional metadata"
+    h2 = hypercat.Hypercat("Fancy Catalogue")
+    h2.supportsSimpleSearch()
+    h2.hasHomepage("http://www.FIXME.com")
+    h2.containsContentType("application/vnd.hypercat.FIXME+json")
+    result = h2.prettyprint()
+    print result
+    assert result=="""{
+    "catalogue-metadata": [
+        {
+            "rel": "urn:X-hypercat:rels:isContentType",
+            "val": "application/vnd.hypercat.catalogue+json"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:hasDescription:en",
+            "val": "Fancy Catalogue"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:supportsSearch",
+            "val": "urn:X-hypercat:search:simple"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:hasHomepage",
+            "val": "http://www.FIXME.com"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:containsContentType",
+            "val": "application/vnd.hypercat.FIXME+json"
+        }
+    ],
+    "items": []
+}"""
+
+    print "\nTEST: Add multiple RELS to a catalogue"
+    h = hypercat.Hypercat("cat")
+    assert h.values("relation") == []
+    h.addRelation("relation","value1")
+    h.addRelation("relation","value2")
+    assert h.values("relation") == ["value1","value2"]
+    print h.prettyprint()
+
+    print "\nTEST: Load a catalogue from a string"
+    inString = """{
+    "catalogue-metadata": [
+        {
+            "rel": "urn:X-hypercat:rels:isContentType",
+            "val": "application/vnd.hypercat.catalogue+json"
+        },
+        {
+            "rel": "urn:X-hypercat:rels:hasDescription:en",
+            "val": "ingestiontestcat"
+        }
+    ],
+    "items": [
+        {
+            "href": "http://FIXME",
+            "item-metadata": [
+                {
+                    "rel": "urn:X-hypercat:rels:isContentType",
+                    "val": "application/vnd.hypercat.catalogue+json"
+                },
+                {
+                    "rel": "urn:X-hypercat:rels:hasDescription:en",
+                    "val": "resource1"
+                }
+            ]
+        },
+        {
+            "href": "http://FIXME2",
+            "item-metadata": [
+                {
+                    "rel": "urn:X-hypercat:rels:isContentType",
+                    "val": "application/vnd.hypercat.catalogue+json"
+                },
+                {
+                    "rel": "urn:X-hypercat:rels:hasDescription:en",
+                    "val": "resource2"
+                }
+            ]
+        },
+        {
+            "href": "http://RESOURCEURL",
+            "item-metadata": [
+                {
+                    "rel": "urn:X-hypercat:rels:isContentType",
+                    "val": "resourcecontenttype"
+                },
+                {
+                    "rel": "urn:X-hypercat:rels:hasDescription:en",
+                    "val": "A resource"
+                }
+            ]
+        }
+    ]
+}"""
+    h = hypercat.loads(inString)
+    outString = h.prettyprint()
+    assert inString == outString
+    print inString
+
+    print "\nUnit tests all passed OK"
+
+if __name__ == "__main__":
+    unittest()
